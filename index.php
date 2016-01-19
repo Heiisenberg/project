8b24a71b61fbb2fb5f73da7c88389f16f1a4a173
lol45d <matidd285@gmail.com>
2016-01-19 06:46:20 +0100

    Commit message

---
+++ index.php
@@ -0,0 +1,35 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Kalkulator 1.0</title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+			<article class="container">
+						<header class="wynik ">
+								<div class="zmienne">4+4</div>
+								<div class="wynik1">=</div>
+								<div class="wynik2">Wynik</div>
+                                <div style="clear:both" ></div> 
+						</header>
+						<article  class="liczby">
+							<div class="zmienna1">1</div>
+							<div class="zmienna2">2</div>
+							<div class="zmienna3">3</div>
+							<div style="clear:both"></div>
+							<div class="zmienna4">4</div>
+							<div class="zmienna5">5</div>
+							<div class="zmienna6">6</div>
+							<div class="zmienna7">7</div>
+							<div class="zmienna8">8</div>
+							<div class="zmienna9">9</div>
+
+						</article>
+					
+
+
+			</article>
+
+
+</body>
+</html>
